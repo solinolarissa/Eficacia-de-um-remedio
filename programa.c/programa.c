7ec060b766f19ad66bff22e0c45b5150a4b0aedc
solinolarissa <66344093+solinolarissa@users.noreply.github.com>
2020-06-28 12:22:21 -0300

    Erro
    Utilizando  bibioteca  #include<stdio.h>
    esta dando erro

---
+++ programa.c/programa.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+int main(){
+
+int populacao [1000][3], i=0, codigo, eficacia, codigo1, cod;
+
+printf("Informe um codigo:");
+
+scanf("%d", &cod);
+
+if(cod == 1){
+
+scanf("%d\n", &populacao[i][0]);
+
+scanf("%d\n", &populacao[i][1]);
+
+populacao[i][2] = -1;
+
+printf("Informe um codigo:");
+
+scanf("%d\n", &codigo);}
+
+if(cod == 2){
+
+for(i = 0; i < 0; i++){
+
+if(codigo == populacao[i][0] || populacao[i][2] == -1){
+
+printf("Erro");}
+
+if(codigo == populacao [i][0] && populacao [i][2] != -1){
+
+printf("Informe a eficacia do remedio");
+
+scanf("%d\n", &eficacia);
+
+populacao [i][2] = eficacia;}}
+
+}
+
+if(cod == 3){
+
+for (i < 1000; i = 0; i++){
+
+if(populacao[i][2]!= -1){
+
+codigo1=(populacao [i][2]*populacao[i][1]/100);
+
+printf("populacao %d = %d\n", populacao[i][0],codigo1);}
+
+}
+
+for(i < 1000; i = 0; i++){
+
+if(populacao[i][2] == 0){
+
+printf("populacao %d = 0\n", populacao[i][0]);}
+
+}}
+
+if(cod == 4){
+
+return 0;
+
+}}
